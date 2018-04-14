ac2dce703e28afdb1b5cf7ff4017dd96152e6882
Antonio Ortiz Pola <rekiem87@gmail.com>
2018-04-14 11:02:11 -0500

    Commit

---
+++ MoneroTickets/MoneroCore/Client/Context/ClientContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneroCore.Client.Context
+{
+    class ClientContext
+    {
+    }
+}
---
+++ MoneroTickets/MoneroTickets/ViewModels/CreateClientAccessRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace MoneroTickets.ViewModels
+{
+    public class CreateClientAccessRequest
+	{
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
